a46e6e4e6f2e6aa31456f2f0412715c7ad3beed8
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2020-05-12 21:14:43 +0200

    changed scene title

---
+++ WAG_No_Sound/Assets/PreEntryTitle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreEntryTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public AudioSource preAudio;
+   public AudioSource loopAudio;
+    void Start()
+    {
+        preAudio.Play();
+        loopAudio.PlayDelayed(preAudio.clip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(!audio.isPlaying)
+        //{
+        //    audio.clip = loopClip;
+        //    audio.loop = true;
+        //    audio.Play();
+        //}
+    }
+}
