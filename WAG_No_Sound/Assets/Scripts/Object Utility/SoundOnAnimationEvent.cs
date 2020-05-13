b28346da79785d8a184412833ddcd36a13e40a81
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-05-13 04:06:34 +0200

    adding some more sounds

--- WAG_No_Sound/Assets/Scripts/Object Utility/SoundOnAnimationEvent.cs
+++ WAG_No_Sound/Assets/Scripts/Object Utility/SoundOnAnimationEvent.cs
@@ -10,9 +10,18 @@ using UnityEngine;
 
 public class SoundOnAnimationEvent : MonoBehaviour {
 
-    public List<AK.Wwise.Event> Sounds = new List<AK.Wwise.Event>();
+    //public List<AK.Wwise.Event> Sounds = new List<AK.Wwise.Event>();
+    public List<AudioClip> Sounds = new List<AudioClip>();
+    private AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void PlaySoundWithIdx(int idx){
-        Sounds[idx].Post(gameObject);
+        //Sounds[idx].Post(gameObject);
+        audioSource.PlayOneShot(Sounds[idx]);
+
     }
 }
